3a7b80558bd866bf57e2d911f75aeb8bd62b2723
Jon Clucas <jon.clucas@childmind.org>
2018-08-12 17:20:21 -0500

    :bookmark: v0.0.7 :books: Add classifiers
    on-behalf-of: @FCP-INDI <http://fcp-indi.github.com>

--- setup.py
+++ setup.py
@@ -14,10 +14,15 @@ from setuptools import find_packages, setup
 
 # Use disutils' setup function to install the package
 setup(name='INDI-Tools',
-      version='0.0.6',
+      version='0.0.7',
       description='Python utilities developed by FCP-INDI',
       author='Daniel Clark',
       author_email='daniel.clark@childmind.org',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3'
+      ],
       url='https://github.com/FCP-INDI/INDI-Tools',
       packages=find_packages(exclude=['test*']),
       install_requires=['botocore', 'boto3'])
